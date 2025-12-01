540c0e40e2b198ec9b105ab3ab16d26ca3c4459f
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-01 12:08:58 -0500

    Created Cmake project

---
+++ Problem1/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Chris Eichert on 12/1/2025.
+//
+
+#include "main.h"
+
+namespace Problem1 {
+} // Problem1
---
+++ Problem1/main.h
@@ -0,0 +1,16 @@
+//
+// Created by Chris Eichert on 12/1/2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+namespace Problem1 {
+
+class main {
+
+};
+
+} // Problem1
+
+#endif //MAIN_H
