f856742cebcfb9d27ef612b559138fe7b9ff0a4b
Christopher Eichert <christopher.eichert@mymail.champlain.edu>
2025-12-29 11:48:01 -0500

    feat(problem 1): Added string parsing and file reading

--- Problem1/main.cpp
+++ Problem1/main.cpp
@@ -4,9 +4,36 @@
 
 #include "main.h"
 
+
 int main() {
 
 
+    //Rotate around the dial from 0-99 and if we go left we subtract
+    //and wrap around, if we go right we add and wrap around
+    //count the number of times we land on zero
 
+    std::ifstream input("../../Problem1/input.txt");
+    int currentRotation = 50;
+    int numberOfZeros = 0;
+
+    if (!input.is_open()) {
+        return -1;
+    }
+
+    while (!input.eof()) {
+
+        //read input
+        std::string rawInput;
+        std::getline(input, rawInput);
+
+        //parse input
+        int value = std::stoi(rawInput.substr(1, rawInput.size() - 1));
+
+        //Add value if it starts with R, otherwise subtract value
+        currentRotation += rawInput[0] == 'R' ? +value : -value;
+    }
+
+
+    input.close();
     return 0;
 }
--- Problem1/main.h
+++ Problem1/main.h
@@ -5,6 +5,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <iostream>
+#include <fstream>
 
 namespace Problem1 {
 
